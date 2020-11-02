0c91be2728c61a0f1d63ba7f7de038dcbf7a773a
Adam Johanides <johanada@fel.cvut.cz>
2020-11-02 09:42:53 +0100

    poc edit, secrets example added

--- scripts/downloadPdf.js
+++ scripts/downloadPdf.js
@@ -1,12 +1,48 @@
+function getSecret(key, secretsEnv) // if needed, this function can contain OAuth autentication.
+{
+    with (new JavaImporter(java.lang.System, java.nio.file))
+    {
+        var secretsPath = System.getenv(secretsEnv)
+        var secretsFileBytes = Files.readAllBytes(Paths.get(secretsPath))
+    }
+
+    var String = Java.type('java.lang.String')
+    var secretsFileString = new String(secretsFileBytes)
+
+    var secretsJson = JSON.parse(secretsFileString)
+    var secret = secretsJson[key]
+
+    return secret.toString()
+}
+
+var token = getSecret('token', 'CAMUNDA_TOKENS_FILE') // get token
+var url = getSecret('url', 'CAMUNDA_TOKENS_FILE') // get url
+
 function downloadFile(fileUrl)
 {
     with (new JavaImporter(org.jsoup, java.io.BufferedInputStream))
     {
 
+        var processDefinitionId = execution.getProcessDefinitionId();
+        var deploymentId = execution.getProcessEngineServices().getRepositoryService().getProcessDefinition(processDefinitionId).getDeploymentId();
+        var resource = execution.getProcessEngineServices().getRepositoryService().getResourceAsStream(deploymentId, 'potvrzeni-o-studiu.xsl');
+        var resourceTestXml = execution.getProcessEngineServices().getRepositoryService().getResourceAsStream(deploymentId, 'xmlTest.xml');
+
+        var IOUtils = Java.type('org.apache.commons.io.IOUtils');
+        var String = Java.type('java.lang.String');
+
+        var xsl = S(new String(IOUtils.toByteArray(resource), 'UTF-8'));
+        var xml = S(new String(IOUtils.toByteArray(resourceTestXml), 'UTF-8'));
+
+        var pdfRequestBody = xsl;
+        pdfRequestBody += "\n__XML_PROPERTIES__\n";
+        pdfRequestBody += xml;
+
         var doc = Jsoup.connect(fileUrl)
             .method(Java.type('org.jsoup.Connection.Method').POST)
-            .header('Accept', 'application/json')
-            .header('Content-Type', 'application/json')
+            .header('Accept', 'text/plain; charset=UTF-8')
+            .header('Content-Type', 'text/plain; charset=UTF-8')
+            .requestBody(pdfRequestBody)
             .timeout(30000)
             .ignoreContentType(true) // This is used because Jsoup "approved" content-types parsing is enabled by default by Jsoup
             .execute()
@@ -47,18 +83,7 @@ function downloadAndSaveFile(fileUrl, fileName, mimeType)
     saveFile(file, fileName, mimeType)
 }
 
-downloadAndSaveFile('http://www.pdf995.com/samples/pdf.pdf', 'mypdf.pdf', 'application/pdf')
+downloadAndSaveFile(url, 'mypdf.pdf', 'application/pdf')
 
-var processDefinitionId = execution.getProcessDefinitionId();
-var deploymentId = execution.getProcessEngineServices().getRepositoryService().getProcessDefinition(processDefinitionId).getDeploymentId();
-var resource = execution.getProcessEngineServices().getRepositoryService().getResourceAsStream(deploymentId, 'potvrzeni-o-studiu.xsl');
-var resourceTestXml = execution.getProcessEngineServices().getRepositoryService().getResourceAsStream(deploymentId, 'xmlTest.xml');
 
-var IOUtils = Java.type('org.apache.commons.io.IOUtils');
-var String = Java.type('java.lang.String');
-
-var xsl = S(new String(IOUtils.toByteArray(resource), 'UTF-8'));
-var xml = S(new String(IOUtils.toByteArray(resourceTestXml), 'UTF-8'));
-
-var pdfRequestBody = xsl.concat("\n__XML_PROPERTIES__\n").concat(xml);
-execution.setVariable('template', xml);
+//execution.setVariable('template', xml);
